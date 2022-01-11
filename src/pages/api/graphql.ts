362e7e26750581f312b6d64dc79d151583b0e78a
Shuntaro Shimizu <ut.s.shimizu@gmail.com>
2022-01-11 14:56:39 +0900

    implement server

--- src/pages/api/graphql.ts
+++ src/pages/api/graphql.ts
@@ -1,12 +1,23 @@
 import type { NextApiRequest, NextApiResponse } from "next";
+import type { Resolvers, User } from "~/graphql/dist/generated-server";
 import { ApolloServer } from "apollo-server-micro";
 import { join } from "path";
-import { Resolvers } from "~/graphql/dist/generated-server";
 import { readFileSync } from "fs";
 
 const path = join(process.cwd(), "src", "graphql", "schema.graphql");
 const typeDefs = readFileSync(path).toString("utf-8");
-const resolvers: Resolvers = {};
+
+const users: User[] = [
+  { id: "1", name: "Alice" },
+  { id: "2", name: "Bob" },
+  { id: "3", name: "Shuntaro Shimizu" },
+];
+const resolvers: Resolvers = {
+  Query: {
+    users: () => users,
+  },
+};
+
 const apolloServer = new ApolloServer({ resolvers, typeDefs });
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
